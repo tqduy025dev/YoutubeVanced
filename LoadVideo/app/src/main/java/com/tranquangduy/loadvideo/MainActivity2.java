f28f98364ef439cf9978b12eb3f628989b2565bb
Your Name <duy05022000@gmail.com>
2021-08-30 19:53:17 +0700

    done download video youtube

--- LoadVideo/app/src/main/java/com/tranquangduy/loadvideo/MainActivity2.java
+++ LoadVideo/app/src/main/java/com/tranquangduy/loadvideo/MainActivity2.java
@@ -1,12 +1,15 @@
 package com.tranquangduy.loadvideo;
 
 
+import android.Manifest;
 import android.app.DownloadManager;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraCharacteristics;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -15,6 +18,7 @@ import android.os.Message;
 import android.os.SystemClock;
 import android.util.SparseArray;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -31,6 +35,7 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 import com.tranquangduy.adapter.CustomAdapter;
 import com.tranquangduy.model.Root;
+import com.yausername.ffmpeg.FFmpeg;
 import com.yausername.youtubedl_android.DownloadProgressCallback;
 import com.yausername.youtubedl_android.YoutubeDL;
 import com.yausername.youtubedl_android.YoutubeDLException;
@@ -47,6 +52,7 @@ public class MainActivity2 extends YouTubeBaseActivity implements YouTubePlayer.
     private ImageButton btnDownload;
     private TextView tvSttDownload;
     private ProgressBar progDownload;
+    private ProgressBar progLoading;
 
     private List<Root.Items> listId = new ArrayList<>();
     private String idVideo = "";
@@ -57,12 +63,41 @@ public class MainActivity2 extends YouTubeBaseActivity implements YouTubePlayer.
     String API_KEY1 = "AIzaSyC-gfCdT68Q50VaItTKL_QqWgI-HsbnV8E";
 
     private long mLastClickTime = 0;
+    private int t = 0;
 
-    Handler mainHandler = new Handler() {
+    private Handler mainHandler = new Handler() {
         @Override
         public void handleMessage(@NonNull Message msg) {
             tvSttDownload.setText(msg.arg1 + "%");
             progDownload.setProgress(msg.arg1);
+            if (msg.arg2 == -1) {
+                Toast.makeText(MainActivity2.this, "Download Thất Bại!", Toast.LENGTH_SHORT).show();
+            }
+            if(msg.obj != null){
+                if(msg.obj.equals("done")){
+                    tvSttDownload.setText("");
+                    progDownload.setProgress(0);
+                    Toast.makeText(MainActivity2.this, "Download Thành Công !", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
+
+    private final DownloadProgressCallback callback = new DownloadProgressCallback() {
+        @Override
+        public void onProgressUpdate(float progress, long etaInSeconds) {
+            System.out.println(progress + "% (ETA " + etaInSeconds + " seconds)");
+            Message msg = mainHandler.obtainMessage();
+            msg.arg1 = (int) progress;
+
+            if( ((int)progress) == 100){
+                t++;
+            }
+            if(t == 2 || t == 3){
+                msg.obj = "done";
+                t = 0;
+            }
+            mainHandler.sendMessage(msg);
         }
     };
 
@@ -96,6 +131,7 @@ public class MainActivity2 extends YouTubeBaseActivity implements YouTubePlayer.
         btnDownload = findViewById(R.id.btnDownload);
         tvSttDownload = findViewById(R.id.tvStatusDownload);
         progDownload = findViewById(R.id.progDownload);
+        progLoading = findViewById(R.id.proLoading);
     }
 
     private void addEvent() {
@@ -104,45 +140,87 @@ public class MainActivity2 extends YouTubeBaseActivity implements YouTubePlayer.
             @Override
             public void onClick(View v) {
 
-                if (SystemClock.elapsedRealtime() - mLastClickTime < 2000){
+                if (SystemClock.elapsedRealtime() - mLastClickTime < 3000) {
                     Toast.makeText(MainActivity2.this, "Nhấn quá nhiều!", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 mLastClickTime = SystemClock.elapsedRealtime();
 
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            startDownload();
-                        } catch (YoutubeDLException | InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
+                checkPermission();
+
             }
         });
+    }
+
+
+    private void checkPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                String[] permission = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permission, 1);
+
+            } else {
+                startDownload();
+            }
+
+        } else {
+            startDownload();
+        }
+
+
+    }
+
+    private void startDownload() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    YoutubeDL.getInstance().init(getApplication());
+                    FFmpeg.getInstance().init(getApplication());
+
+                    File youtubeDLDir = getDownloadLocation();
+                    YoutubeDLRequest request = new YoutubeDLRequest(urlDownload);
+                    request.addOption("-o", youtubeDLDir.getAbsolutePath() + "/%(title)s.%(ext)s");
+
+//                    YoutubeDL.getInstance().execute(request, (progress, etaInSeconds) -> {
+//                        System.out.println(progress + "% (ETA " + etaInSeconds + " seconds)");
+//                        Message msg = mainHandler.obtainMessage();
+//                        msg.arg1 = (int) progress;
+//                        mainHandler.sendMessage(msg);
+//                        callback.onProgressUpdate(progress, etaInSeconds);
+//                    });
+
+                    YoutubeDL.getInstance().execute(request, callback);
+
+
+                } catch (YoutubeDLException | InterruptedException e) {
+                    e.printStackTrace();
+                    Message msg = mainHandler.obtainMessage();
+                    msg.arg2 = -1;
+                    mainHandler.sendMessage(msg);
+                }
+
+
+            }
+        }).start();
 
     }
 
 
-    private void startDownload() throws YoutubeDLException, InterruptedException {
-        YoutubeDL.getInstance().init(getApplication());
-        YoutubeDL.getInstance().updateYoutubeDL(getApplication());
 
-        File youtubeDLDir = getDownloadLocation();
-        YoutubeDLRequest request = new YoutubeDLRequest(urlDownload);
-        request.addOption("-o", youtubeDLDir.getAbsolutePath() + "/%(title)s.%(ext)s");
-
-        YoutubeDL.getInstance().execute(request, (progress, etaInSeconds) -> {
-            System.out.println(progress + "% (ETA " + etaInSeconds + " seconds)");
-            Message msg = mainHandler.obtainMessage();
-            msg.arg1 = (int) progress;
-            mainHandler.sendMessage(msg);
-        });
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startDownload();
+            } else {
+                Toast.makeText(this, "Chưa cấp quyền!!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
-
 
 
     @NonNull
@@ -168,7 +246,6 @@ public class MainActivity2 extends YouTubeBaseActivity implements YouTubePlayer.
                     urlDownload = "";
                     urlDownload = urlPath.concat(idVideo);
 
-
                     //start foreground
 //                    Intent intent = new Intent(MainActivity2.this, MyService.class);
 //                    Song song = new Song(items.snippet.title,items.snippet.thumbnails.high.url,1);
@@ -189,8 +266,6 @@ public class MainActivity2 extends YouTubeBaseActivity implements YouTubePlayer.
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
         Toast.makeText(this, "Init fail", Toast.LENGTH_SHORT).show();
     }
-
-
 
 
 }
